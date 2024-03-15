05524432be2f3efbccc5a528d5696d37548d5ec4
ishantgill <ishant1645@gmail.com>
2024-03-15 11:46:29 +0530

    this is a star pattern code

--- ishant.c
+++ ishant.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 int main(){
-  int rows;
+  int rows, n, r = 1;
   printf("Enter the number of rows = ");
   scanf("%d", &rows);
+    n = rows;
+  for (int i = 1; i <= rows; i++){
+    for (int j = n-1; j >= 1; j--){
+      printf("\t");
+    }
+    n--;
+    for(int k = 1; k <= r; k++){
+        printf("*\t");
+    }
+    r++;
+    printf("\n");
+  }
   return 0;
 }
